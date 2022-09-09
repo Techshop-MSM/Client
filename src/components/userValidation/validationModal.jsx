be5d3a8fd69b1e64a29a1bd45e3e1fc782a26735
Best-Web-Dev <hokenholz@gmx.de>
2022-09-09 14:15:16 +0200

    Pull Modal

--- src/components/userValidation/validationModal.jsx
+++ src/components/userValidation/validationModal.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react"
 import { Login } from "./login/login"
 import { Register } from "./register/register"
+import { nav, Button, Container, Col, Row, Form, Modal, Tabs,Tab } from "react-bootstrap";
+
 
 export const ValidationModal = () => {
 
