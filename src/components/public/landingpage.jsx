6fb519eda9da1ab5c7f193a385b9767f7ad7dbae
Martin <martinkr90@googlemail.com>
2022-09-12 11:03:29 +0200

    before pull

--- src/components/public/landingpage.jsx
+++ src/components/public/landingpage.jsx
@@ -1,27 +1,29 @@
-import { Topmenu } from './Topmenu'
-import { TopCarousel } from './TopCarousel'
-import { TopSellings } from './TopSellings'
-import { TrendingProducts } from './TrendingProducts'
-import { KürlichHinzugefügt } from './KürzlichHinzugefügt'
+import { Topmenu } from '../reusables/parts/Topmenu'
+import { TopCarousel } from './landingpageAssets/TopCarousel'
+import { TopSellings } from './landingpageAssets/TopSellings'
+import { TrendingProducts } from './landingpageAssets/TrendingProducts'
+import { KürlichHinzugefügt } from './landingpageAssets/KürzlichHinzugefügt'
 import { BestBewertet } from './BestBewertet'
-import { Footer } from './Footer'
-import { MegaMenu } from './MegaMenu'
-import { ShoppingCartPreview } from './ShoppingCartPreview'
+import { Footer } from '../reusables/parts/Footer'
+import { MegaMenu } from '../reusables/parts/MegaMenu'
+import { ShoppingCartPreview } from '../reusables/parts/ShoppingCartPreview'
 
 export const Home = () => {
-return (
-    <>
-
-    <Topmenu/>
-    <ShoppingCartPreview/>
-    <MegaMenu/>
-    <TopCarousel/>
-    <img src='https://www.alternate.de/tw/mar-86201/teaser/desktop/MAR-86201_Cont_d.jpg' className='img-Fluid'></img>
-    <TopSellings/>
-    <TrendingProducts/>
-    <KürlichHinzugefügt/>
-    <BestBewertet/>
-    <Footer/>
-    </>
-)
+    return (
+        <>
+            <Topmenu />
+            <ShoppingCartPreview />
+            <MegaMenu />
+            <TopCarousel />
+            <img
+                src="https://www.alternate.de/tw/mar-86201/teaser/desktop/MAR-86201_Cont_d.jpg"
+                className="img-Fluid"
+            ></img>
+            <TopSellings />
+            <TrendingProducts />
+            <KürlichHinzugefügt />
+            <BestBewertet />
+            <Footer />
+        </>
+    )
 }
