6709ea1547fc3ee31a820936bbe88786146da114
MartinK <82144965+MartinKruess@users.noreply.github.com>
2022-09-12 10:17:43 +0200

    Testuser: Login and register (#23)
    * added .dio as a company diagramm
    
    * routung & privat routes fist steps
    
    * components prepared
    
    * register private and company
    
    * add company reigster
    
    * validation modal finished
    
    * validation Frontend Test - success
    
    * Testuser created, Login with Username or Email possible
    
    * fixed changes modal
    
    Co-authored-by: m.poettgen@gmx.net <82958985+poettas@users.noreply.github.com>
    Co-authored-by: Martin <martinkr90@googlemail.com>

--- src/components/userValidation/validationModal.jsx
+++ src/components/userValidation/validationModal.jsx
@@ -1,20 +1,19 @@
-import { useState } from "react"
-import { Login } from "./login/login"
-import { Register } from "./register/register"
-
+import { useState } from 'react'
+import { Login } from './login/login'
+import { Register } from './register/register'
 
 export const ValidationModal = () => {
-
     const [modalToggle, setModalToggle] = useState(true)
 
     return (
         <div>
             <Nav>
-                <div onClick={(()=> setModalToggle(!modalToggle))}>LOGIN</div>
-                <div onClick={(()=> setModalToggle(!modalToggle))}>REGISTRIEREN</div>
+                <div onClick={() => setModalToggle(!modalToggle)}>LOGIN</div>
+                <div onClick={() => setModalToggle(!modalToggle)}>
+                    REGISTRIEREN
+                </div>
             </Nav>
             {modalToggle ? <Login /> : <Register />}
         </div>
-        
     )
 }
